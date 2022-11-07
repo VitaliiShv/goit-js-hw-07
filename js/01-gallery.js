1fda5b6c6ba97ba3204d9a3bbe1d39453c308817
Vitalii Shved <vitalyashved@gmail.com>
2022-11-07 23:50:30 +0200

    finished 01-gallery
    finished 01-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,11 +2,16 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const galleryContainerEl = document.querySelector(".gallery");
+
 const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+
+let modalInstance = null;
 
 galleryContainerEl.insertAdjacentHTML("beforeend", galleryMarkup);
 
-galleryContainerEl.addEventListener("click", onGalleryContainerClick);
+galleryContainerEl.addEventListener("click", onGalleryImageClick);
+
+// window.addEventListener("keydown", onEscapeKeyPress);
 
 function createGalleryItemsMarkup(galleryItems) {
   return galleryItems
@@ -25,16 +30,26 @@ function createGalleryItemsMarkup(galleryItems) {
     .join("");
 }
 
-function onGalleryContainerClick(event) {
+function onGalleryImageClick(event) {
   event.preventDefault();
+  window.addEventListener("keydown", onEscapeKeyPress);
+  if (event.currentTarget === event.target) {
+    return;
+  }
 
-  basicLightbox
-    .create(
-      `<img
+  modalInstance = basicLightbox.create(
+    `<img
       class="gallery__image"
       src="${event.target.dataset.source}"
-      
-    />`
-    )
-    .show();
+          />`
+  );
+
+  modalInstance.show();
+}
+
+function onEscapeKeyPress(event) {
+  if (event.code === "Escape") {
+    window.removeEventListener("keydown", onEscapeKeyPress);
+    modalInstance.close();
+  }
 }
